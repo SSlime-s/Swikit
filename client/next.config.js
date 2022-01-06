a05f3ea3b585a4af3b4e2238749a66995ec7f648
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-01-07 00:48:26 +0900

    🎉 home が見れるように

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+
+  webpack(config) {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  },
 }
