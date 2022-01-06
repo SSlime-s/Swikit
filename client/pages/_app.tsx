f31d99d1c42037923c123cf617d3ac1891a53421
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-01-07 07:09:56 +0900

    🗑️ apollo が残ってた

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,25 +4,18 @@ import type { AppProps } from 'next/app'
 import { Layout } from 'antd'
 import Search from 'antd/lib/input/Search'
 import 'windi.css'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const { Header } = Layout
 
-const client = new ApolloClient({
-  uri: "http://localhost:8088/graphql",
-  cache: new InMemoryCache(),
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <Header className='bg-white flex items-center'>
-          <Search placeholder='search' allowClear className='max-w-80' />
-        </Header>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    <Layout>
+      <Header className='bg-white flex items-center'>
+        <Search placeholder='search' allowClear className='max-w-80' />
+      </Header>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
